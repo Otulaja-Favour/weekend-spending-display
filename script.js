ee63b5a99a1e5d20f06ab6a230681bef6ae14ae2
Otulaja favour <otulajafavour14@gmail.com>
2025-03-15 14:39:57 -0700

    mini expense calculator

---
+++ script.js
@@ -0,0 +1,31 @@
+//to get the input value that will be inputted in the user interface for the calculation of expense
+document.getElementById('eachExpenses').addEventListener('input', function () {
+
+
+    //calling the input value from the interface
+    let list = document.getElementById('eachExpenses').value;
+
+    // console.log(list)
+    //adding up the value from the interface
+    let addUp = eval(list);
+    // console.log(addUp)
+
+
+    //i  need to retun it to show my output in the userinterface
+    //EXPENSES
+    function equal(addUp) {
+        return addUp
+    }
+    document.getElementById('spent').innerHTML = 'Total Expenses =' + ' ' + equal(addUp) + 'naira'
+    // console.log('Total Expenses =' + ' ' + (equal(addUp)) + 'naira')
+
+
+    // the remainder 
+    const budget = document.getElementById('budget').value;
+    let amount = budget
+    console.log(amount)
+    let remain = amount - equal(addUp);
+    console.log(remain)
+    document.getElementById('remainder').innerHTML = 'The amount remaining from your budget is' + ' ' + remain
+
+})
